eff8823f648d61050dcbed897c2aee30dd30ed69
jão <juaun1981@alu.ufc.br>
2025-09-11 09:42:09 -0300

    feat: test 1

---
+++ Rest-with-Spring/src/main/java/com/github/jaum1981/models/Greeting.java
@@ -0,0 +1,10 @@
+package com.github.jaum1981.models;
+
+public record Greeting(long id, String content) {
+
+//    private long id;
+//    private String content;
+//
+//    public Greeting(long id, String content) {
+//    }
+}
---
+++ Rest-with-Spring/src/test/java/com/github/jaum1981/RestWithSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.jaum1981;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWithSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
